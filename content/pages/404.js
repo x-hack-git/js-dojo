18d5f8a2d9c8565945568769d746d696374bd8c8
松田信介 <>
2021-02-11 17:43:45 +0900

    トップページ追加

---
+++ content/pages/404.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { graphql } from "gatsby"
+const siteTitle = `Title`
+
+return (
+  <Layout location={location} title={siteTitle}>
+    <SEO title="All posts" />
+    <Bio />
+    <div>
+      エラー
+    </div>
+  </Layout>
+)
