1965b679b4b91a15b3cc12a3eb2755364c4c71ed
shinsukematsuda <smatsuda@x-hack.jp>
2021-02-14 19:11:08 +0900

    画像ファイル修正

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,21 @@
 import React from "react"
+import Container from "../components/container"
+import Header from "../components/header/header"
+import Footer from "../components/footer/footer"
+import MainCategory from "../components/main_category/index"
+import Main from "../components/main_contents/index"
 
 export default function About() {
   return (
     <div>
-      Error.
+      <Header />
+      <Container>
+        <h1>存在しないページです</h1>
+        <p>
+          URLをご確認ください
+        </p>
+      </Container>
+      <Footer />
     </div>
   )
 }
