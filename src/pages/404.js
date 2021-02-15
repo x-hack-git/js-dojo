3af6b979a0b221f0d64b12cd4e68929a7a37c2e7
松田信介 <>
2021-02-15 18:51:24 +0900

    ファイル名修正

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,8 +2,6 @@ import React from "react"
 import Container from "../components/container"
 import Header from "../components/header/header"
 import Footer from "../components/footer/footer"
-// import MainCategory from "../components/main_category/index"
-// import Main from "../components/main_contents/index"
 
 export default function About() {
   return (
