a4371c1603eb56399d1ca2f9dc171a8d5b97089f
shinsukematsuda <smatsuda@x-hack.jp>
2021-02-16 12:20:04 +0900

    チュートリアルページの修正

--- src/components/javascript_basics/tutorials.js
+++ src/components/javascript_basics/tutorials.js
@@ -1,25 +1,49 @@
 import React from "react"
 import { useStaticQuery, graphql, Link } from "gatsby"
+// import Image from "gatsby-image"
+// import styled from "styled-components"
+
+// const BannerImage = styled(Image)`
+//   border-radius: 10px;
+//   max-width: 400px;
+//   margin-top: 20px;
+// `
 
 export default props => {
-  const { allMdxPage } = useStaticQuery(graphql`
-    query {
-      allMdxPage(filter: {slug: {regex: "/day[0-9]{2}/"}}, sort: {order: ASC, fields: slug}) {
-        edges {
-          node {
-            slug
+  const { allMdx } = useStaticQuery(graphql`
+    query MyQuery {
+      allMdx (filter: {slug: {regex: "/javascript_basics\/tutorials/"}}) {
+        nodes {
+          slug
+          frontmatter {
             title
+            slug
+            tags
+            date
+            banner
           }
         }
       }
     }
   `)
 
+  console.log(allMdx.nodes)
+
   return (
-    allMdxPage.edges.map(_ => {
+    allMdx.nodes.map(_ => {
       return (
-        <Link key={_.node.title} to={_.node.slug}>
-          <p>{_.node.title}</p>
+        <Link key={_.frontmatter.title} to={_.frontmatter.slug}>
+          <p style={{ backgroundColor: `purple` }}>
+            {/* {_.frontmatter.banner && (
+              <BannerImage
+                // fluid={mdx.frontmatter.banner.childImageSharp.fluid}
+                alt="Banner Image"
+              />
+            )} */}
+            <img src={_.frontmatter.banner} className="" style={{ width: `240px` }}></img>
+            <h4>{_.frontmatter.title}</h4>
+          </p>
+          {/* <img src={_.frontmatter.banner.childImageSharp.fluid} className="" style={{ width: `240px` }}></img> */}
         </Link>
       )
     })
