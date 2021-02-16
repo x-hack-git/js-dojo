318b7cba1adaadf9d3a122a0b79d302bb29dbd91
shinsukematsuda <smatsuda@x-hack.jp>
2021-02-17 00:09:30 +0900

    テーマを上書き

---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,14 @@
+import baseTheme from "@lekoarts/gatsby-theme-minimal-blog/src/gatsby-plugin-theme-ui"
+
+export default {
+  ...baseTheme,
+  colors: {
+    text: "#333",
+    background: "#fff",
+    primary: "tomato",
+  },
+  fonts: {
+    body: "Georgia, serif",
+    heading: "system-ui, sans-serif",
+  },
+}
