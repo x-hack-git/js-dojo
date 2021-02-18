8e5dd0a7fa787c64984e099ee2063d27a175790b
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2021-02-18 19:21:30 +0900

    containerのスタイルを追加

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -4,14 +4,54 @@ export default {
   ...baseTheme,
   styles: {
     h1: {
-      fontSize: [28, 40],
-      background: 'linear-gradient(transparent 70%, #FFFF80 70%)',
-      display: 'inline'
+      fontSize: [20, 40]
     },
     h2: {
-      fontSize: [24, 32],
+      fontSize: [24],
       background: 'linear-gradient(transparent 70%, #FFFF80 70%)',
-      display: 'inline'
+      display: 'inline',
+    },
+    blockquote: {
+      borderLeftColor: `primary`,
+      borderLeftStyle: `solid`,
+      borderLeftWidth: `6px`,
+      mx: 0,
+      pl: 4,
+      p: {
+        fontStyle: `italic`,
+      },
+    },
+    table: {
+      width: `100%`,
+      my: 4,
+      borderCollapse: `separate`,
+      borderSpacing: 0,
+      [[`th`, `td`]]: {
+        textAlign: `left`,
+        py: `4px`,
+        pr: `4px`,
+        pl: 0,
+        borderColor: `muted`,
+        borderBottomStyle: `solid`,
+      },
+    },
+    th: {
+      verticalAlign: `bottom`,
+      borderBottomWidth: `2px`,
+      color: `heading`,
+    },
+    td: {
+      verticalAlign: `top`,
+      borderBottomWidth: `1px`,
+    },
+    hr: {
+      mx: 0,
+    },
+  },
+  layout: {
+    container: {
+      padding: [3, 4],
+      maxWidth: `756px`,
     },
   }
 }
