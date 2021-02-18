da4fe1defa4f4fb58c061a44d47fc3418dba6e8e
gakisan8273 <mint.daa.a2@gmail.com>
2021-02-19 05:12:53 +0900

    改行

--- src/components/button/signout.js
+++ src/components/button/signout.js
@@ -3,5 +3,12 @@ import buttonStyles from "./button.module.css"
 import { auth } from "gatsby-theme-firebase";
 
 export default function SignOut() {
-  return <span className={buttonStyles.logout} onClick={() => auth.signOut()}>Sign out</span>
+  return (
+    <span
+      className={buttonStyles.logout}
+      onClick={() => auth.signOut()}
+    >
+      Sign out
+    </span>
+  );
 }
